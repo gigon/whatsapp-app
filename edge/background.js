e14a82f2e560306f4a39e080e7d3ea89edc633fe
Gil <ggonen@synamedia.com>
2024-09-26 22:51:54 +0300

    Support +972 numbers

--- edge/background.js
+++ edge/background.js
@@ -14,17 +14,17 @@ chrome.contextMenus.create({
         let phoneNumber = match[0].replace(/[-.\s()]/g, '');
         if (!phoneNumber.startsWith('+')) {
           const countryCode = '972'; // Default to 972
-          if (!phoneNumber.startsWith('00')) {
-            phoneNumber = `${countryCode}${phoneNumber}`;
-          } else {
-            phoneNumber = phoneNumber.replace(/^00/, '');
+          if (phoneNumber.startsWith('0')) {
+            phoneNumber = phoneNumber.substring(1); // Remove leading zero
           }
-          const whatsappUrl = `https://wa.me/${phoneNumber}`;
-          chrome.tabs.create({ url: whatsappUrl });
+          if (!phoneNumber.startsWith('972')) {
+            phoneNumber = `${countryCode}${phoneNumber}`;
+          }
         } else {
-          const whatsappUrl = `https://wa.me/${phoneNumber.replace('+', '')}`;
-          chrome.tabs.create({ url: whatsappUrl });
+          phoneNumber = phoneNumber.replace('+', ''); // Remove the plus sign
         }
+        const whatsappUrl = `https://wa.me/${phoneNumber}`;
+        chrome.tabs.create({ url: whatsappUrl });
       } else {
         alert("No valid Israeli mobile number found in the selected text.");
       }
